da68771b31f575593ade9917a0f0fc1a267f9a5d
clebersantz <clebersantz@gmail.com>
2025-05-08 23:50:08 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 mkdir odoo16-prod && cd odoo16-prod
 
 # Clone Odoo 16 source
-git clone -b 16.0 https://github.com/odoo/odoo.git odoo
+git clone --depth --branch 16.0 https://github.com/odoo/odoo.git odoo
 
 # Create addons directory & clone OCA modules
 mkdir addons
 cd addons
-git clone -b 16.0 https://github.com/OCA/l10n-brazil.git
-git clone -b 16.0 https://github.com/OCA/account-financial-tools.git
+git clone --depth 1 --branch 16.0 https://github.com/OCA/l10n-brazil.git
+git clone --depth 1 --branch 16.0 https://github.com/OCA/account-financial-tools.git
 cd ..
